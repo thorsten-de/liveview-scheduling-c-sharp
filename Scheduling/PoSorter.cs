15ea6894a747eb470523e0b8cb769b9c39b4d85e
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-10-26 15:00:47 +0200

    Implement TopoSort and VerifySort

---
+++ Scheduling/PoSorter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduling
+{
+  internal static class EnumerableExtensions
+  {
+    public static void ForEach<T>(this IEnumerable<T> @this, Action<T> action)
+    {
+      foreach (var item in @this)
+        action(item);
+    }
+  }
+
+  internal class PoSorter
+  {
+    public IList<Task> SortedTasks { get; private set; }
+
+    public IList<Task> TopoSort(IList<Task> taskList)
+    {
+      foreach (var task in taskList)
+      {
+        task.PrereqCount = task.PrereqTasks.Count;
+        task.PrereqTasks.ForEach(pre => pre.AddFollower(task));
+      }
+
+      var sortedTasks = new List<Task>(taskList.Count);
+      var readyTasks = new Queue<Task>();
+
+      taskList
+        .Where(t => t.PrereqCount == 0)
+        .ForEach(readyTasks.Enqueue);
+
+      while (readyTasks.Any())
+      {
+        var readyTask = readyTasks.Dequeue();
+        sortedTasks.Add(readyTask);
+
+        foreach (var follower in readyTask.Followers)
+        {
+          follower.PrereqCount--;
+          if (follower.PrereqCount == 0)
+            readyTasks.Enqueue(follower);
+        }
+      }
+
+      SortedTasks = sortedTasks;
+      return sortedTasks;
+    }
+
+    public bool VerifySort() =>
+      SortedTasks.All(appearAfterPrereq);
+
+    private static bool appearAfterPrereq(Task t) =>
+      t.PrereqTasks.All(pre => pre.Index < t.Index);
+  }
+}
--- Scheduling/Task.cs
+++ Scheduling/Task.cs
@@ -11,15 +11,21 @@ namespace Scheduling
     public int Index { get; private set; }
     public string Name { get; private set; }
     
+
     public IList<int> PrereqNumbers { get; private set; }
 
     public IList<Task> PrereqTasks { get; private set; }
+
+    public int PrereqCount { get; set; }
+    public IList<Task> Followers { get; private set; }
+
 
     public Task(int index, string name, IEnumerable<int> prereqNumbers)
     {
       Index = index;
       Name = name;
       PrereqNumbers = new List<int>(prereqNumbers);
+      Followers = new List<Task>();
     }
 
     public void NumbersToTasks(IList<Task> taskList)
@@ -29,6 +35,8 @@ namespace Scheduling
           .Select(index => taskList[index])
           .ToList();
     }
+
+    public void AddFollower(Task follower) => Followers.Add(follower);
 
     public override string ToString() => Name;
   }
